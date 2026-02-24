7e7e9639b6f3a3189ac60ed0920e567b3be787f2
Marcel du Preez <dupreez.marcel@gmail.com>
2026-02-24 09:48:12 +0000

    feat: add slack report generator api (v0.0.87)

--- api/function_app.py
+++ api/function_app.py
@@ -12,3 +12,5 @@ from blueprints.health import bp as health_bp
 app.register_functions(health_bp)
 from blueprints.reporting import bp as reporting_bp
 app.register_functions(reporting_bp)
+from blueprints.slack_report import bp as slack_report_bp
+app.register_functions(slack_report_bp)
